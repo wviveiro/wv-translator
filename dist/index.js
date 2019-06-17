22bce5858b34b1b500ed30abb9370e27aae340d3
Wellington Viveiro <wellyviveiro@gmail.com>
2019-06-17 10:24:08 +1000

    add distribution

--- dist/index.js
+++ dist/index.js
@@ -5,15 +5,58 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = require("react");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-var Component = function Component() {
-  return _react.default.createElement("div", {
-    className: "my-first-component"
-  }, _react.default.createElement("h2", null, "This is my Component!"), _react.default.createElement("hr", null), _react.default.createElement("p", null, "Hello World! Edit this file to build your own component"), _react.default.createElement("h4", null, "Tips to build your own component:"), _react.default.createElement("hr", null), _react.default.createElement("ol", null, _react.default.createElement("li", null, _react.default.createElement("strong", null, "Javascript/React code:"), " All the code present in the folder ", _react.default.createElement("code", null, "src/lib"), " will be compiled and added to the folder ", _react.default.createElement("code", null, "dist"), ".", _react.default.createElement("br", null), "Your ", _react.default.createElement("code", null, "package.json"), " has been set with ", _react.default.createElement("code", null, "dist/index.js"), " as main script."), _react.default.createElement("li", null, _react.default.createElement("strong", null, "Your scss files:"), " All css you need has to be present in the file ", _react.default.createElement("code", null, "src/styles/index.scss"), ". You can import other scss files in this file if you want to split your code.", _react.default.createElement("br", null), "The css will be compiled and added to the folder ", _react.default.createElement("code", null, "dist/styles/index.css")), _react.default.createElement("li", null, _react.default.createElement("strong", null, "The Playground:"), " the playgrond is used to test your component. the files ", _react.default.createElement("code", null, "src/lib/index.jsx"), " and ", _react.default.createElement("code", null, "src/styles/index.scss"), " are included in the playground, so you can test your component with ", _react.default.createElement("code", null, "npm start")), _react.default.createElement("li", null, _react.default.createElement("strong", null, "Building my component:"), " Start editing your ", _react.default.createElement("code", null, "src/lib/index.jsx"), " and ", _react.default.createElement("code", null, "src/styles/index.scss"), " to build your component! Once your finish, run ", _react.default.createElement("code", null, "npm run build"), " to compile your component.")), _react.default.createElement("p", null, "You can publish this entire project. The ", _react.default.createElement("strong", null, "development"), " dependencies are ignored through ", _react.default.createElement("code", null, ".gitignore"), " and ", _react.default.createElement("code", null, ".npmignore")), _react.default.createElement("p", null, "Happy hack!"));
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var translate = function translate(dictionary) {
+  return function (phrase) {
+    if (!phrase) return '';
+    var t = dictionary.find(function (d) {
+      return d.from === phrase;
+    });
+
+    if (process.env.NODE_ENV === 'development' && !t) {
+      console.log('Translation not found for: ', phrase);
+    }
+
+    return t && t.to ? t.to : phrase;
+  };
 };
 
-var _default = Component;
+var createDictionary = function createDictionary(dictionary) {
+  var t = translate(dictionary);
+
+  var Translator = function Translator(props) {
+    var value = props.value;
+
+    var _useState = (0, _react.useState)(''),
+        _useState2 = _slicedToArray(_useState, 2),
+        translation = _useState2[0],
+        setTranslation = _useState2[1];
+
+    var _useState3 = (0, _react.useState)(''),
+        _useState4 = _slicedToArray(_useState3, 2),
+        realValue = _useState4[0],
+        setRealValue = _useState4[1];
+
+    (0, _react.useEffect)(function () {
+      if (value !== realValue) {
+        setRealValue(value);
+        setTranslation(t(value));
+      }
+    }, [value, realValue]);
+    return translation;
+  };
+
+  return Translator;
+};
+
+var _default = createDictionary;
 exports.default = _default;
